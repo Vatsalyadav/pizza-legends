c9ff3567d50dbf6ead63b840f5724ba7f8e7358b
Vatsal Yadav <vt769414@dal.ca>
2022-04-14 04:59:28 -0300

    Initial JS description and element use explanations

--- Overworld.js
+++ Overworld.js
@@ -1,16 +1,22 @@
 class Overworld {
+ //   will monitor the states and act as top level component for the canvas tab
  constructor(config) {
+   //  save the canvas details and context
    this.element = config.element;
+   // gets the container and then access the game canvas within it
    this.canvas = this.element.querySelector(".game-canvas");
    this.ctx = this.canvas.getContext("2d");
  }
 
  init() {
+     // 1. for canvas, an image needs to be loaded in memory so create an object. No need to inject it in dom
    const image = new Image();
+   // 3. It'll load on our canvas using its context
    image.onload = () => {
-     this.ctx.drawImage(image,0,0)
+     this.ctx.drawImage(image,0,0) // source, x, y coordinate
    };
    image.src = "/images/maps/DemoLower.png";
+   // 2. Once the image is download -> 3
 
 
    const x = 5;
--- init.js
+++ init.js
@@ -2,7 +2,9 @@
 
   const overworld = new Overworld({
     element: document.querySelector(".game-container")
+  //  provide details to the Overworld constructor
   });
   overworld.init();
 
 })();
+// this function calls itself (fn(){...})();
