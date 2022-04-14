a73d9392274830671540d4191d925ab6f4da650e
Vatsal Yadav <vt769414@dal.ca>
2022-04-14 11:24:20 -0300

    Hero and shadow canvas parameters

--- Overworld.js
+++ Overworld.js
@@ -9,6 +9,8 @@ class Overworld {
  }
 
  init() {
+     /* Imp Info: The canvas draws things one over another by the order of arrival,
+     so ordering of created image objects is important */
      // 1. for canvas, an image needs to be loaded in memory so create an object. No need to inject it in dom
    const image = new Image();
    // 3. It'll load on our canvas using its context
@@ -19,8 +21,8 @@ class Overworld {
    // 2. Once the image is download -> 3
 
 
-   const x = 5;
-   const y = 6;
+   const x = 5; // current position multiplier
+   const y = 6; // change them to move character around
 
    const shadow = new Image();
    shadow.onload = () => {
@@ -39,17 +41,18 @@ class Overworld {
    shadow.src = "/images/characters/shadow.png";
 
 
+   // Hero object is a sprite sheet so we need to apply cropping
    const hero = new Image();
    hero.onload = () => {
      this.ctx.drawImage(
        hero, 
-       0, //left cut 
+       0, //left cut
        0, //top cut,
-       32, //width of cut
+       32, //width of cut since the single frame is 32x32 so, stop cropping at 32px
        32, //height of cut
-       x * 16 - 8,
+       x * 16 - 8, // 16 is the tile size on our background image, 8 is the nudging applied to centre the character
        y * 16 - 18,
-       32,
+       32, // aspect ratios
        32
     )
    }
